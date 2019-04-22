c841d72710661178899aab1177050e611c26809b
Ramil <soroka250402@yandex.ru>
2019-04-22 16:00:08 +0400

    Bookmarks are now available
    Bookmarks controll skill is now added. User can add, delete and look at bookmarks.

--- handler.py
+++ handler.py
@@ -28,7 +28,16 @@ def init_route(app, db):
 
     # What bot can do for user:
     possibilities = [
-        '*Nothing here now*'
+        '1)Show random book',
+        '2)Show book called *title*',
+        '3)Show book by *author*',
+        '4)Show book in *category* category',
+        '5)Add book called *title* by *author* to wish/reading now/already read list',
+        '6)Delete book called *title* by *author* from wish/reading now/already read list',
+        '7)Show all books from wish/reading now/already list',
+        '8)Add bookmark in book called *title* by *author* on page *page* and name it *title*',
+        '9)Delete bookmark from book called *title* by *author* on page *page*',
+        '10)Show bookmarks from book called *title* by *author*'
     ]
 
     # Post request decorator
@@ -93,7 +102,7 @@ def init_route(app, db):
                     name = name.capitalize()
                     # New user creating
                     User.add(session_storage['alice_id'], name)
-                    res['response']['text'] = 'Nice to meet you, {}! That\'s what I can:\n{}'.format(name, '\n'.join(possibilities))
+                    res['response']['text'] = 'Nice to meet you, {}! That\'s what I can:\n{}'.format(name, '\n\n\n'.join(possibilities))
                     session_storage['username'] = name
                     session_storage['current_theme'] = 'what_to_do'
 
@@ -103,7 +112,7 @@ def init_route(app, db):
                     # If one of positive words is in request word list
                     if word in req_words:
                         res['response']['text'] = 'That\'s what I can:\n{}'.format(
-                            '\n'.join(possibilities)
+                            '\n\n\n'.join(possibilities)
                         ) + '\nWhat do you want me to do?'
                         session_storage['current_theme'] = 'what_to_do'
                 for word in ['no']:
@@ -138,7 +147,7 @@ def init_route(app, db):
                             book_author = ' '.join(
                                 req_words[req_words.index('by') + 1:req_words.index('to'):]
                             ).capitalize()
-                        elif 'from' in req_words:
+                        elif 'from' in req_words and 'bookmarks' not in req_words:
                             book_author = ' '.join(
                                 req_words[req_words.index('by') + 1:req_words.index('from'):]
                             ).capitalize()
@@ -163,6 +172,87 @@ def init_route(app, db):
                             response_text = get_response_text_from_book(book)
                         res['response']['text'] = response_text + '\n\n\nWhat else I can do for you, {}?'.format(session_storage['username'])
 
+                    elif 'bookmark' in req_words and 'page' in req_words:
+                        bookmark_page, bookmark_title = '', ''
+                        for num in range(len(req_words)):
+                            # Trying to find bookmark page
+                            if req_words[num] == 'on' and req_words[num + 1] == 'page':
+                                bookmark_page = req_words[num + 2]
+                            # Trying to find bookmark title
+                            if 'name' in req_words:
+                                if req_words[num] == 'name' and req_words[num + 1] == 'it':
+                                    bookmark_title = ' '.join(req_words[num + 2::])
+
+                        if 'add' in req_words:
+                            # If all data is correct
+                            if book_name and book_author and bookmark_page and bookmark_title:
+                                book = Book.query.filter_by(
+                                    title=book_name,
+                                    author=book_author
+                                ).first()
+                                # If book in any list
+                                if book:
+                                    # If book in now reading list
+                                    if book.status == 1:
+                                        Bookmark.add(book, bookmark_page, bookmark_title)
+                                        res['response']['text'] = 'Bookmark has been added!' + '\n\n\nWhat else I can do for you, {}?'.format(session_storage['username'])
+                                    # If book not in now reading list
+                                    else:
+                                        res['response']['text'] = 'Chosen book should be in \"Reading now\" list!'
+                                # If book not found in any of lists
+                                else:
+                                    res['response']['text'] = 'No such book found!'
+                        elif 'delete' in req_words:
+                            # If all data is correct
+                            if book_name and book_author and bookmark_page:
+                                book = Book.query.filter_by(
+                                    title=book_name,
+                                    author=book_author
+                                ).first()
+                                # If book in any list
+                                if book:
+                                    # If book in now reading list
+                                    if book.status == 1:
+                                        bookmark = Bookmark.query.filter_by(
+                                            book_id=book.id,
+                                            page=bookmark_page
+                                        ).first()
+                                        Bookmark.delete(bookmark)
+                                        res['response']['text'] = 'Bookmark has been deleted!' + '\n\n\nWhat else I can do for you, {}?'.format(session_storage['username'])
+                                    # If book not in now reading list
+                                    else:
+                                        res['response']['text'] = 'Chosen book should be in \"Reading now\" list!'
+                                # If book not found in any of lists
+                                else:
+                                    res['response']['text'] = 'No such book found!'
+                    elif 'bookmarks' in req_words and 'from' in req_words:
+                        # Getting book
+                        print(book_name, book_author)
+                        book = Book.query.filter_by(
+                            title=book_name,
+                            author=book_author
+                        ).first()
+                        if book:
+                            # Getting bookmarks list Show bookmarks from book called *title* by *author*
+                            bookmarks = [book for book in Bookmark.query.filter_by(
+                                book_id=book.id
+                            )]
+                            # If chosen book has bookmarks in it
+                            print(bookmarks)
+                            if bookmarks:
+                                response_text = '\n\n\n'.join([
+                                    'Page:\n{}\n\n\nTitle:\n{}'.format(
+                                        bookmark.page,
+                                        bookmark.title
+                                    ) for bookmark in bookmarks
+                                ])
+                                res['response']['text'] = response_text
+                            # If no bookmarks found in chosen book
+                            else:
+                                res['response']['text'] = 'No bookmarks in this book!'
+                        else:
+                            response_text = 'Specified book not found!'
+                            res['response']['text'] = response_text
                     # If user wants to add book in the list
                     elif 'list' in req_words:
                         # Getting list type
@@ -182,24 +272,30 @@ def init_route(app, db):
                         if 'add' in req_words and 'to' in req_words:
                             # If all data is correct
                             if book_name and book_author and list_type != -1:
+                                # Getting user
                                 user = User.query.filter_by(
                                     alice_id=session_storage['alice_id']
                                 ).first()
+                                # Getting book
                                 book = Book.query.filter_by(
                                     title=book_name,
                                     author=book_author
                                 ).first()
+                                # If book already in some list
                                 if book:
                                     response_text = 'Book already in \"{}\" list!'.format(
                                         ['Wish', 'Now reading', 'Already read'][book.status]
                                     )
                                     res['response']['text'] = response_text
+                                # If book not in lists
                                 else:
                                     books = list(Book.query.filter_by(
                                         status=list_type
                                     ))
+                                    # If list consists from 5 books
                                     if len(books) >= 5:
                                         res['response']['text'] = 'You can\'t add more than 5 books in one list!'
+                                    # If list consists from less than 5 books
                                     else:
                                         Book.add(user, book_name, book_author, list_type)
                                         res['response']['text'] = 'Book has been added!' + '\n\n\nWhat else I can do for you, {}?'.format(session_storage['username'])
@@ -236,7 +332,7 @@ def init_route(app, db):
                             res['response']['text'] = response_text + '\n\n\nWhat else I can do for you, {}?'.format(session_storage['username'])
                             session_storage['current_theme'] = 'what_to_do'
 
-                if 'books' in req_words and 'from' in req_words and 'list' in req_words:
+                elif 'books' in req_words and 'from' in req_words and 'list' in req_words:
                     # Getting list type
                         list_type = -1
                         if 'wish' in req_words:
@@ -276,7 +372,7 @@ def init_route(app, db):
                 # If user asks to tell him what bot can do
                 elif 'what' in req_words and 'can' in req_words:
                     res['response']['text'] = 'That\'s what I can:\n{}'.format(
-                        '\n'.join(possibilities)
+                        '\n\n\n'.join(possibilities)
                     ) + '\nWhat do you want me to do?'
         except Exception as e:
             print(e)
